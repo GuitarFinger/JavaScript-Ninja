a0fc6b854447d7ba44799142fdbc291bb6d62ee7
HZC <815547360@qq.com>
2018-03-06 23:00:33 +0800

    注释

--- Chapter-6/subclass.js
+++ Chapter-6/subclass.js
@@ -3,9 +3,14 @@
  */
 (function () {
     var initializing = false,
+        /*如果函数能够正常序列化(test()方法将接收一个字符串,然后触发函数的toString()方法)
+        * 最终结果返回true, 用superPattern来判断一个函数是否包含字符串“_super”.只有函数支持
+        * 序列化才能判断,所以在不支持序列化的浏览器上,使用一个匹配任意字符串的模式进行替换
+        * */
         superPattern = /xyz/.test(function() { xyz; }) ? /\b_super\b/ : /.*/;
 
     Object.subClass = function (properties) {
+        // 子类实例化: properties,该参数是期望添加到子类的属性集
         var _super = this.prototype;
 
         initializing = true;
