8ebf1998140e77e88030e894a8aa4d94070385f4
HZC <815547360@qq.com>
2018-05-13 22:47:16 +0800

    修改bug

--- scripts/assert.js
+++ scripts/assert.js
@@ -1,13 +1,13 @@
 function assert(value, desc) {
     var results = document.querySelector("#results");
     if (!results) {
-        resultsList = document.createElement('ul');
-        document.getElementsByTagName('body')[0].appendChild(resultsList);
-        resultsList.setAttribute('id', 'results');
+        results = document.createElement('ul');
+        document.getElementsByTagName('body')[0].appendChild(results);
+        results.setAttribute('id', 'results');
     }
 
     var li = document.createElement("li");
     li.className = value ? "pass" : "fail";
     li.appendChild(document.createTextNode(desc));
-    resultsList.appendChild(li);
+    results.appendChild(li);
 }
