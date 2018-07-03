26f7fa4f0749a87dc2c558db46824f94bdcf992e
HZC <815547360@qq.com>
2018-07-03 12:25:27 +0800

    用click或keypress对submit进行冒泡测试

---
+++ Chapter-13/event-support.js
@@ -0,0 +1,16 @@
+/**事件冒泡检测代码*/
+function isEventSupported(eventName) {
+    var element = document.createElement('div'),
+        isSupported;
+
+    eventName = 'on' + eventName;
+    isSupported = (eventName in element);
+
+    if (!isSupported) {
+        element.setAttribute(eventName, 'return;');
+        isSupported = typeof element[eventName] == 'function';
+    }
+
+    element = null;
+    return isSupported;
+}
